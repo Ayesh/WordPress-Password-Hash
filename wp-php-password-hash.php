d8f90bdd89f2029c59768263cb64985bddd27a04
My1 <My1@users.noreply.github.com>
2017-12-31 16:53:22 +0100

    Fix class 'PasswordHash' not loaded at the password verification step

--- wp-php-password-hash.php
+++ wp-php-password-hash.php
@@ -57,6 +57,11 @@ function wp_check_password( $password, $hash, $user_id = '' ) {
     return apply_filters( 'check_password', $check, $password, $hash, $user_id );
   }
 
+  //if class doesnt exist, pull it in
+  if(!class_exists("PasswordHash")) {
+    require_once ABSPATH . WPINC . '/class-phpass.php';
+  }
+
   // If the stored hash is longer than an MD5, presume the
   // new style phpass portable hash.
   if ( empty($wp_hasher) ) {
