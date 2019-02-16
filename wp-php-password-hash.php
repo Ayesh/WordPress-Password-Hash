4cd87ae918900f5ff3438395b5cf149f9cd15bfb
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2019-02-16 13:53:13 +0530

    Update author URL to https://ayesh.me/open-source

--- wp-php-password-hash.php
+++ wp-php-password-hash.php
@@ -5,7 +5,7 @@
  * Description: Swaps out WordPress's password hashing mechanism with PHP 5.5's `password_hash()` functions set, and automatically rehashes the existing passwords on users next successful login. Provides safety against dictionary attacks, time-attacks, brute-force attacks.
  * Licence:     GPLv2 or later
  * Author:      Ayesh Karunaratne
- * Author URI:  https://ayesh.me
+ * Author URI:  https://ayesh.me/open-source
  */
 
 if (function_exists('wp_hash_password')) {
