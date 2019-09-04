acfc2971766628f3ccc2098c0fb7605ca8f114ea
Ayesh Karunaratne <ayesh@ayesh.me>
2019-09-04 23:23:37 +0800

    Update stable tag

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ayeshrajans
 Tags: password, password hashing, password_hash, bcrypt, argon2, argon2i, argon2id, sodium, password security, security
 Requires at least: 3.9.2
 Tested up to: 5.3
-Stable tag: 2.0
+Stable tag: 2.1
 Requires PHP: 5.5
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- wp-php-password-hash.php
+++ wp-php-password-hash.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: PHP native password hash
- * Version:     2.0
+ * Version:     2.1
  * Description: Swaps out WordPress's password hashing mechanism with PHP 5.5's `password_hash()` functions set, and automatically rehashes the existing passwords on users next successful login. Provides safety against dictionary attacks, time-attacks, brute-force attacks.
  * Licence:     GPLv2 or later
  * Author:      Ayesh Karunaratne
